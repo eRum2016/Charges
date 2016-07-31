3c8f15e98052ab096363dd4c0a3dadf5444496ae
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2016-07-31 23:58:37 +0200

    kto i ILE

--- who_payed_from_eRum_xlsx.R
+++ who_payed_from_eRum_xlsx.R
@@ -6,12 +6,21 @@ read.xlsx('data/eRum.xlsx', rows = 11:605) -> eRum
 eRum[c((1:148)*4-1), ] -> eRum
 
 
-eRum[!grepl("OPŁ.BANK.|ZLECONE", eRum) ] %>%
+eRum[!grepl("OPŁ.BANK.|ZLECONE|ODSETKI", eRum) ] %>%
   gsub(pattern = "[0-9,-]|/EUR/|WB|ERUM|EUR|eRUM|eRum|WORKSHOP", "", .) %>%
   stri_extract_all_words() %>%
   lapply(paste, collapse = " ") %>%
-  unlist %>%
-  unique %>%
+  unlist -> kto
+
+
+eRum[!grepl("OPŁ.BANK.|ZLECONE|ODSETKI", eRum) ] %>%
+  gsub(pattern = "699,58USD", "", .) %>%
+  stri_extract_all_regex("([.0-9]+,[0-9]+)") %>%
+  unlist -> ile
+
+library(dplyr)
+data.frame(kto, ile) %>%
+  arrange(kto) %>%
   write.table(file = "data/who_payed.txt", 
               quote = FALSE, 
               row.names = FALSE, 
